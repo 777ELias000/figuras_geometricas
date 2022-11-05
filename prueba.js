775aa25a7860d84229969bf2c4a0933df8ced1e3
777ELias000 <112776027+777ELias000@users.noreply.github.com>
2022-11-05 05:05:14 +0100

    Update prueba.js

--- prueba.js
+++ prueba.js
@@ -14,22 +14,22 @@ switch(figura){
 
         base=prompt("Cuanto tienes de base ")
         altura=prompt("Cuanto tienes de altura ")
-        document.write(`Tu area del rectangulo es ${base*altura} cm`)
+        document.write(`Tu area del rectangulo es ${base*altura}cm²`)
     break
     case "2":
         radio=prompt("Cuanto tiene de radio")
         //Math 
-        document.write(`El area de tu circulo es ${Math.PI*Math.pow(radio,2)} cm`)
+        document.write(`El area de tu circulo es ${Math.PI*Math.pow(radio,2)} cm²`)
     break
     case "3":
         base = prompt("Cuanto tienes de base")
         altura=prompt("Cuanto tienes de altura")
-        document.write(`El area del triangulo: ${base*altura /2}cm`)
+        document.write(`El area del triangulo: ${base*altura /2}cm²`)
     break
     case"4":
         base = prompt("Cuanto tienes de base")
         altura=prompt("Cuanto tienes de altura")
-        document.write(`El area del Paralelogramo es: ${base*altura}cm`)
+        document.write(`El area del Paralelogramo es: ${base*altura}cm²`)
     break
     case"5":
         lado= prompt("Cuanto mide el lado")
@@ -43,13 +43,13 @@ switch(figura){
     case"7":
         diagonal_a= prompt("Cuanto es tu diagonal a")
         diagonal_b=prompt("Cuanto es tu diagonal b")
-        document.write(`El area del Rombo es${diagonal_a*diagonal_b/2}cm`)
+        document.write(`El area del Rombo es${diagonal_a*diagonal_b/2}cm²`)
     break
     case"8":
        base1=parseInt(prompt("Ingresar la base mayor"))
        base2=parseInt(prompt("Ingresar la  base menor"))
        altura=prompt("Cuanto tienes de altura")
-       document.write(`El area del Trapecio es${altura*((base1 + base2)/2)}cm`)
+       document.write(`El area del Trapecio es${altura*((base1 + base2)/2)}cm²`)
 
     break   
 
